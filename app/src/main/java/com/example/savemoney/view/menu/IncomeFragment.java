8366b3740a35fa983338835a8794c4bdf4cb0d0b
ttangutts <01689674504zx@gmail.com>
2023-05-10 14:12:38 +0700

    Ttan: add navigation

--- app/src/main/java/com/example/savemoney/view/menu/IncomeFragment.java
+++ app/src/main/java/com/example/savemoney/view/menu/IncomeFragment.java
@@ -2,19 +2,29 @@ package com.example.savemoney.view.menu;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.savemoney.R;
+import com.example.savemoney.databinding.FragmentIncomeBinding;
 
 public class IncomeFragment extends Fragment {
+    FragmentIncomeBinding binding;
+    NavController controller;
+    Button btnToHistoryIOFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
@@ -22,6 +32,16 @@ public class IncomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_income, container, false);
+        controller = Navigation.findNavController(requireActivity(),R.id.nav_host_fragment_container);
+        binding = FragmentIncomeBinding.inflate(inflater,container,false);
+        return binding.getRoot();    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnToHistoryIOFragment = binding.btnDetail;
+        btnToHistoryIOFragment.setOnClickListener(v->{
+            controller.navigate(R.id.historyIOFragment);
+        });
     }
 }
